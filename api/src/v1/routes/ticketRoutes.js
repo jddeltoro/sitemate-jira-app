f5fb031085d1fdd2885e91d6f00caeedd4afa4f5
jddeltoro <jdiazdeltoro@gmail.com>
2024-03-11 05:41:56 -0400

    add crud routes

--- api/src/v1/routes/ticketRoutes.js
+++ api/src/v1/routes/ticketRoutes.js
@@ -3,3 +3,15 @@ const apicache = require('apicache');
 const router = express.Router();
 const cache = apicache.middleware;
 const workoutController = require('../controllers/ticketController');
+
+router.get('/', cache("2 minutes"), workoutController.getAllTickets);
+
+router.get('/:id', workoutController.getTicketById);
+
+router.post('/', workoutController.createTicket);
+
+router.patch('/:id', workoutController.updateTicket);
+
+router.delete('/:id', workoutController.deleteTicket);
+
+module.exports = router;
