e4249b8066d50402079b0cc9046bf8efc711547a
jddeltoro <jdiazdeltoro@gmail.com>
2024-03-11 06:46:35 -0400

    update express to use cors

--- api/src/index.js
+++ api/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const v1TicketsRouter = require('./v1/routes/ticketRoutes');
 const { swaggerDocs: V1SwaggerDocs } = require('./v1/swagger');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
 app.use('/api/v1/tickets', v1TicketsRouter);
